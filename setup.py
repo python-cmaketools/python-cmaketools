4df8484cd3d71a969ad4e0a406b7991faa867a94
Takeshi Ikuma <tikuma@gmail.com>
2020-06-05 23:18:20 -0500

    Fixed description & license metadata

--- setup.py
+++ setup.py
@@ -2,12 +2,22 @@
 
 from setuptools import setup
 
+
+# read the contents of your README file
+from os import path
+
+with open("README.md", encoding="utf-8") as f:
+    long_description = f.read()
+
 setup(
     name="cmaketools",
-    version="0.1.0",
+    version="0.1.2",
     author="Takeshi (Kesh) Ikuma",
     author_email="tikuma@gmail.com",
     description="Seamless integration of Cmake build system to setuptools/distutils",
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    license="MIT License",
     url="https://github.com/python-cmaketools/python-cmaketools",
-    packages=["cmaketools"]
+    packages=["cmaketools"],
 )
